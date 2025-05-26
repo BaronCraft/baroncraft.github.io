3a46c8d97229bc646975a74e0ad72f5b6b65562b
BaronCraft <mkomissarov488012011@gmail.com>
2025-05-26 18:15:00 +0300

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,5 +1,5 @@
 let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
+let themeButton = document.querySelector('.theme');
 
 themeButton.onclick = function() {
   page.classList.toggle('light-theme');
